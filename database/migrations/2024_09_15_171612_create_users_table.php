362d3d54648c1b65390977fde3c064eb95d28501
shoaib-nizam <shoaibnizam44@gmail.com>
2026-06-19 00:01:45 +0500

    19-6-2026

--- database/migrations/2024_09_15_171612_create_users_table.php
+++ database/migrations/2024_09_15_171612_create_users_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',['user','admin'])->default('user');
-            $table->timestamps();
-        });
+       Schema::create('users', function (Blueprint $table) {
+    $table->id();
+    $table->string('name');
+    $table->unsignedBigInteger('banquet_owner_id')->nullable();
+    $table->foreign('banquet_owner_id')->references('banquet_id')->on('banquats')->onDelete('cascade');
+    $table->string('email')->unique();
+    $table->string('password');
+    $table->enum('role', ['user', 'admin'])->default('user');
+    $table->timestamps();
+});
     }
 
     /**
