37d8106cf6ebb9ecd3baadc3e0358a90e0beef87
shoaib-nizam <shoaibnizam44@gmail.com>
2026-06-19 00:22:49 +0500

    19-6-2026

--- resources/views/admin/manage.blade.php
+++ resources/views/admin/manage.blade.php
@@ -3,6 +3,27 @@
 @section('content')
 
 
+    <div class="container-fluid border border-dark">
+            <div class="row">
+                <div class="col-4 m-auto">
+                    <h1>All Record</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-6">
+
+                    
+
+                </div>
+
+                <div class="col-6">
+
+                </div>
+            </div>
+
+    </div>
+
 
 
 @endsection
