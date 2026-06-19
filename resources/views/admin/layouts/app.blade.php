6be4c62f8a1f809416a514548e6e63a6ded56cb2
shoaib-nizam <shoaibnizam44@gmail.com>
2026-06-20 00:10:23 +0500

    19-6-2026

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -217,7 +217,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('manage') }}" class="nav-link">
+                <a href="{{ route('manage_show') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Owner & Banquet</p>
                 </a>
